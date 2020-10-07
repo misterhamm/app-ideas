88530cdc19b39413290dc68ba6943958592b70bc
Chris Hamm <chamm@bizstream.com>
2020-10-06 23:55:31 -0400

    all inputs working, comments added

--- Projects/1-Beginner/Border-Radius-Previewer/app.js
+++ Projects/1-Beginner/Border-Radius-Previewer/app.js
@@ -12,33 +12,86 @@ const cssOutput = document.getElementById('css-output');
 const showAdvBtn = document.getElementById('show-adv');
 const advContainer = document.getElementById('advanced');
 
+// Set up vars for advanced border radii
+let hrtl = '';
+let hrtr = '';
+let hrbr = '';
+let hrbl = '';
+let vrtl = '';
+let vrtr = '';
+let vrbr = '';
+let vrbl = '';
+
+
+// When one of the basic inputs is changed, grab them in order and apply the border radius accordingly
 basicInputs.forEach((input) => {
-  input.oninput = function () {
+    input.oninput = function() {
+        // build the declaration string
+        let basicBR = `${basicInputs[0].value || 0} ${basicInputs[1].value || 0} ${basicInputs[2].value || 0} ${basicInputs[3].value || 0}`;
 
-    let basicBR = `${basicInputs[0].value || 0}px ${basicInputs[1].value || 0}px ${basicInputs[2].value || 0}px ${basicInputs[3].value || 0}px`;
+        // zero out the advanced inputs
+        advInputs.forEach((input) => {
+            input.value = 0;
+        });
 
-    advInputs.forEach((input) => {
-      input.value = 0;
-    });
-    radiusBox.style.borderRadius = basicBR;
-    cssOutput.innerHTML = `border-radius: ${basicBR};`;
-  }
+        // set the style
+        radiusBox.style.borderRadius = basicBR;
+
+        // update the display box
+        cssOutput.innerHTML = `border-radius: ${basicBR};`;
+    }
 });
 
+// When one of the advanced inputs is changed, check the id, and set the right var property
+// Dear god I'm sure there is a better way to do this and I'm staring right at it but can't see
 advInputs.forEach((input) => {
-  input.oninput = function () {
+    input.oninput = function() {
+        // set border radius of correct one
+        switch (input.id) {
+          case 'hrtl':
+              hrtl = input.value;
+              break;
+          case 'hrtr':
+              hrtr = input.value;
+              break;
+          case 'hrbr':
+              hrbr = input.value;
+              break;
+          case 'hrbl':
+              hrbl = input.value;
+              break;
+          case 'vrtl':
+              vrtl = input.value;
+              break;
+          case 'vrtr':
+              vrtr = input.value;
+              break;
+          case 'vrbr':
+              vrbr = input.value;
+              break;
+          case 'vrbl':
+              vrbl = input.value;
+              break;
+          default:
+              break;
+      }
 
-    let advBR = `${advInputs[0].value || 0}px ${advInputs[1].value || 0}px ${advInputs[2].value || 0}px ${advInputs[3].value || 0}px / ${advInputs[4].value || 0}px ${advInputs[5].value || 0}px ${advInputs[6].value || 0}px ${advInputs[7].value || 0}px`;
-    console.log(advBR)
-    basicInputs.forEach((input) => {
-      input.value = 0;
-    });
+      //build our declaration string
+      let advBR = `${hrtl || 0} ${hrtr || 0} ${hrbr || 0} ${hrbl || 0} / ${vrtl || 0} ${vrtr || 0} ${vrbr || 0} ${vrbl || 0}`;
+      
+      // zero out basic inputs
+      basicInputs.forEach((input) => {
+          input.value = 0;
+      });
 
-    radiusBox.style.borderRadius = advBR;
-    cssOutput.innerHTML = `border-radius: ${advBR};`;
+      // actually set the style
+      radiusBox.style.borderRadius = advBR;
+      // update the display box
+      cssOutput.innerHTML = `border-radius: ${advBR};`;
   }
 });
 
-showAdvBtn.onclick = function () {
-  advContainer.style.display = 'block';
+// show advanced inputs
+showAdvBtn.onclick = function() {
+    advContainer.style.display = 'block';
 }
